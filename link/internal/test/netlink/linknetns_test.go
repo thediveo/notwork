997e10a920fbc4a6653708d9c935f604eeaa3b90
thediveo <thediveo@gmx.eu>
2024-07-26 21:54:37 +0200

    test: internal netlink assertions and documentation
    Signed-off-by: thediveo <thediveo@gmx.eu>

---
+++ link/internal/test/netlink/linknetns_test.go
@@ -0,0 +1,70 @@
+// Copyright 2024 Harald Albrecht.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package netlink
+
+import (
+	"os"
+	"time"
+
+	"github.com/thediveo/notwork/link"
+	"github.com/thediveo/notwork/netns"
+	"github.com/vishvananda/netlink"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gleak"
+	. "github.com/thediveo/fdooze"
+	. "github.com/thediveo/success"
+)
+
+var _ = Describe("netlink network namespace handling", func() {
+
+	BeforeEach(func() {
+		if os.Getuid() != 0 {
+			Skip("needs root")
+		}
+		goodfds := Filedescriptors()
+		goodgos := Goroutines()
+		DeferCleanup(func() {
+			Eventually(Goroutines).Within(2 * time.Second).ProbeEvery(250 * time.Millisecond).
+				ShouldNot(HaveLeaked(goodgos))
+			Expect(Filedescriptors()).NotTo(HaveLeakedFds(goodfds))
+		})
+	})
+
+	Context("vishvananda/netlink", func() {
+
+		It("fails to fetch the index of a new link created in a different network namespace", func() {
+			netnsfd := netns.NewTransient()
+
+			l := &netlink.Dummy{
+				LinkAttrs: netlink.LinkAttrs{
+					Name:      link.RandomNifname("dmy-"),
+					Namespace: netlink.NsFd(netnsfd),
+				},
+			}
+			Expect(netlink.LinkAdd(l)).To(Succeed())
+			Expect(l.Attrs().Index).To(BeZero())
+
+			Expect(Successful(
+				netns.NewNetlinkHandle(netnsfd).LinkByName(l.Name))).To(
+				HaveField("Attrs()", And(
+					HaveField("Name", l.Attrs().Name),
+					HaveField("Index", Not(BeZero())))))
+		})
+
+	})
+
+})
