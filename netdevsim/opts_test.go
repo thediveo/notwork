b0574e1fbb19c82e3f214466eb83b75d3ca739c6
thediveo <thediveo@gmx.eu>
2024-07-29 14:13:03 +0200

    refact: netdevsim, macvlan
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- netdevsim/opts_test.go
+++ netdevsim/opts_test.go
@@ -12,29 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package macvlan
+package netdevsim
 
 import (
-	"github.com/thediveo/notwork/link"
-	"github.com/vishvananda/netlink"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("MACVLAN configuration options", func() {
 
-	It("configures MACVLAN", func() {
-		l := &link.Link{Link: &netlink.Macvlan{}}
+	It("configures veth", func() {
+		o := &Options{}
 		for _, opt := range []Opt{
 			InNamespace(42),
-			WithMode(netlink.MACVLAN_MODE_VEPA),
+			WithID(123),
+			WithPorts(10),
+			WithRxTxQueueCountEach(666),
 		} {
-			Expect(opt(l)).To(Succeed())
+			Expect(opt(o)).To(Succeed())
 		}
-		Expect(InNamespace(-42)(l)).To(Succeed())
-		Expect(l.Link).To(HaveField("Namespace", netlink.NsFd(-42)))
-		Expect(l.Link).To(HaveField("Mode", netlink.MACVLAN_MODE_VEPA))
+		Expect(o.NetnsFd).To(Equal(42))
+		Expect(o.HasID).To(BeTrue())
+		Expect(o.ID).To(Equal(uint(123)))
+		Expect(o.Ports).To(Equal(uint(10)))
+		Expect(o.QueueCount).To(Equal(uint(666)))
 	})
 
 })
