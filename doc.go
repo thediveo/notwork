9ed13d67574802198f2194a36f581b3ad0724a74
thediveo <thediveo@gmx.eu>
2024-07-30 13:06:31 +0200

    chore: documentation
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
-Package notwork helps unit tests to create transient virtual network interfaces.
-It leverages both the [Ginkgo] testing framework and matchting (erm, sic!)
-[Gomega] matchers.
+Package notwork is a tiny package that helps unit tests to create transient
+virtual network interfaces. It leverages [vishvananda/netlink], as well as the
+[Ginkgo] testing framework with [Gomega] matchers.
 
 # Usage Example
 
@@ -36,20 +36,20 @@ Even better, don't trash around the host network namespace, but instead use a
 throw-away network namespace that is separate from the host network namespace.
 
 	import (
-		"github.com/thediveo/notwork/dummy"
-		"github.com/thediveo/notwork/macvlan"
-		"github.com/thediveo/notwork/netns"
+	    "github.com/thediveo/notwork/dummy"
+	    "github.com/thediveo/notwork/macvlan"
+	    "github.com/thediveo/notwork/netns"
 
-		. "github.com/onsi/ginkgo/v2"
-		. "github.com/onsi/gomega"
+	    . "github.com/onsi/ginkgo/v2"
+	    . "github.com/onsi/gomega"
 	)
 
 	var _ = Describe("some isolated testing", func() {
 
-		It("creates a transient MACVLAN with a dummy parent inside a throw-away netns", func() {
-			defer netns.EnterTransient()()
-			mcvlan := macvlan.NewTransient(dummy.NewTransient())
-		})
+	    It("creates a transient MACVLAN with a dummy parent inside a throw-away netns", func() {
+	        defer netns.EnterTransient()()
+	        mcvlan := macvlan.NewTransient(dummy.NewTransient())
+	    })
 
 	})
 
@@ -62,26 +62,52 @@ With the previous examples under our black notwork belts, let's create a VETH
 pair of network interfaces that connect two transient network namespaces.
 
 	import (
-		"github.com/notwork/netns"
-		"github.com/notwork/veth"
+	    "github.com/notwork/netns"
+	    "github.com/notwork/veth"
 	)
 
 	It("connects two temporary network namespaces", func() {
-		dupondNetns := netns.NewTransient()
-		dupontNetns := netns.NewTransient()
-		var dupond, dupont netlink.Link
-		netns.Execute(dupondNetns, func() {
-			dupond, dupont = veth.NewTransient(WithPeerNamespace(dupontNetns))
-		})
+	    dupondNetns := netns.NewTransient()
+	    dupontNetns := netns.NewTransient()
+	    var dupond, dupont netlink.Link
+	    netns.Execute(dupondNetns, func() {
+	        dupond, dupont = veth.NewTransient(WithPeerNamespace(dupontNetns))
+	    })
 	})
 
 As for the names of the VETH pair end variables, please refer to [Dupond et
 Dupont].
 
+# Testing /sys/class/net (Sysfs)
+
+Sometimes, the (RT)NETLINK API isn't enough and you need to test production code
+that uses information inside “/sys/class/net”. Unfortunately, sysfs is
+peculiar in that its network view doesn't adapt to a viewer's current network
+namespace. Instead, sysfs freezes the network namespace to display when it gets
+mounted: from then on, it shows the mounter's network namespace that the caller
+was attached to when issuing the mount syscall.
+
+This module thus supports creating transient mount namespaces and mounting a
+correct sysfs instance.
+
+	import (
+		"github.com/notwork/mntns"
+		"github.com/notwork/netns"
+	)
+
+	It("creates transient mount and network namespaces, mounts a sysfs", func() {
+		defer netns.EnterTransient()()
+		mntnsfd, procfsroot := mntns.NewTransient()
+		mntns.Execute(mntnsfd, func() {
+			mntns.MountSysfsRO()
+		}
+	})
+
 # Known Limitations
 
 This module suffers from overzealous sub-packaging.
 
+[vishvananda/netlink]: https://github.com/vishvananda/netlink
 [Ginkgo]: https://github.com/onsi/ginkgo
 [Gomega]: https://github.com/onsi/gomega
 [MACVLAN]: https://developers.redhat.com/blog/2018/10/22/introduction-to-linux-interfaces-for-virtual-networking#macvlan
