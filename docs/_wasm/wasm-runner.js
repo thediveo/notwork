22c22a8ec3ca7b6b223a77b5dd83e32e8e307d58
thediveo <thediveo@gmx.eu>
2026-04-10 18:23:16 +0000

    wip
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- docs/_wasm/wasm-runner.js
+++ docs/_wasm/wasm-runner.js
@@ -1,55 +1,56 @@
 const ANSI = {
-  reset: '\x1b[0m',
+    reset: '\x1b[0m',
 
-  bold: '\x1b[1m',
+    bold: '\x1b[1m',
 
-  green: '\x1b[32m',
-  red: '\x1b[31m',
-  yellow: '\x1b[33m',
-  cyan: '\x1b[36m',
-  gray: '\x1b[90m'
+    green: '\x1b[32m',
+    red: '\x1b[31m',
+    yellow: '\x1b[33m',
+    cyan: '\x1b[36m',
+    gray: '\x1b[38;5;250m'
 }
 
 function color(text, ...styles) {
-  return styles.join('') + text + ANSI.reset
+    return styles.join('') + text + ANSI.reset
 }
 
 function createTerminal(containerId) {
-  const term = new Terminal({
-    cols: 80,
-    rows: 20,
-    convertEol: true,
-  })
+    const term = new Terminal({
+        cols: 80,
+        rows: 20,
+        convertEol: true,
+    })
 
-  const el = document.getElementById(containerId);
-  el.innerHTML = ''
-  term.open(el)
+    const el = document.getElementById(containerId);
+    el.innerHTML = ''
+    term.open(el)
 
-  return term
+    return term
 }
 
 function runWasmProgram(containerId, wasmFile) {
-  const term = createTerminal(containerId)
+    const term = createTerminal(containerId)
 
-  term.writeln('[' + wasmFile + ']\n')
+    term.writeln(color(`▶ Starting ${wasmFile}...\n`, ANSI.gray))
 
-  const worker = new Worker('_wasm/wasm-worker.js')
+    const worker = new Worker('_wasm/wasm-worker.js')
 
-  worker.onmessage = (e) => {
-    const msg = e.data
+    worker.onmessage = (e) => {
+        const msg = e.data
 
-    if (msg.type === 'stdout') {
-      term.write(msg.data)
-    }
+        if (msg.type === 'stdout') {
+            term.write(msg.data)
+        }
 
-    if (msg.type === 'error') {
-      term.writeln('\n[error]\n' + msg.data)
-    }
+        if (msg.type === 'error') {
+            term.writeln('\n' + color('✖ error\n' + msg.data, ANSI.bold, ANSI.red))
+        }
 
-    if (msg.type === 'done') {
-      term.writeln('\n[finished]')
-    }
-  };
+        if (msg.type === 'done') {
+            term.writeln('\n' + color('✓ Finished', ANSI.gray)
+            )
+        }
+    };
 
-  worker.postMessage({ wasm: wasmFile })
+    worker.postMessage({ wasm: wasmFile })
 }
